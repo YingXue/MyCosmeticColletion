d1bfb84f94cce1e76faf0854f891b5ee6a78cc34
ying xue <yingxue@microsoft.com>
2017-04-28 16:04:16 -0700

    add table and style

--- myCtrl.js
+++ myCtrl.js
@@ -1,16 +1,22 @@
 app.controller("myCtrl", function($scope) {
-    $scope.productOwned= 50;
     $scope.productName = "lotion";
     $scope.productPrice = 0;
     $scope.productCatalog = "";
     $scope.productReview = "";
+    $scope.productOwned = [
+	    {name:"fresh loction", catalog:"skin care/face"}, 
+	    {name:"la mer eye concentrate", catalog:"skin care/eye"}, 
+	    {name:"too faced peach eyeshodow", catalog:"cosmetics/eye"} , 
+	    {name:"amarni fundation", catalog:"cosmetics/face"} , 
+	    {name:"sephora conture powder", catalog:"cosmetics/face"} 
+    ];
 
     $scope.purchase = function(){
     	return "Ying just bought " + $scope.productName + " for $" + $scope.productPrice;
     }
 
     $scope.history = function(){
-    	return "Ying already bought " + $scope.productOwned + " products";
+    	return "Ying already bought " + $scope.productOwned.length + " products";
     }
 });
 
