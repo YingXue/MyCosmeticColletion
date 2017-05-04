9c4169c67493ccc94f8ce022793fbd79cc30422c
ying xue <yingxue@microsoft.com>
2017-05-03 18:13:25 -0700

    add purchase date

--- myCtrl.js
+++ myCtrl.js
@@ -3,18 +3,18 @@ app.controller("myCtrl", ['$scope', '$http',function($scope,$http) {
     $scope.productPrice = 0;
     $scope.productCatalog = "";
     $scope.productOwnedSummary = "";
-    
+    $scope.purchaseDate = "";
     //productOwned need to read file json file
     $scope.productOwned = [
-        {"name":"La Mer Eye Concentrate", "catalog":"Skincare / Eye", "price":"180", "dateofpurchase":"2017-03"}, 
-        {"name":"Cle De Peau Correcting Cream Veil", "catalog":"Cosmetics / Face", "price":"70", "dateofpurchase":"2017-03"},
-	    {"name":"Laura Mercier Pressed Powder", "catalog":"Cosmetics / Face", "price":"56", "dateofpurchase":"2017-03"},
-        {"name":"Giorgio Armani Eyeshaper", "catalog":"Cosmetics / Tool", "price":"25", "dateofpurchase":"2017-03"},
-        {"name":"Marc Jacobs Matte Eyeliner", "catalog":"Cosmetics / Eye", "price":"20", "dateofpurchase":"2017-03"},
-        {"name":"Fresh Deep Hydration Facial Toner", "catalog":"Skincare / Face", "price":"40", "dateofpurchase":"2017-04"}, 
-	    {"name":"Nars Dual-intensity Eyeshadow", "catalog":"Cosmetics / Eye", "price":"26", "dateofpurchase":"2017-04"} , 
-	    {"name":"givenchy ink fundation", "catalog":"Cosmetics / Face", "price":"60", "dateofpurchase":"2017-05"} , 
-	    {"name":"Sephora Colorful Face Powders", "catalog":"Cosmetics / Face", "price":"14", "dateofpurchase":"2017-04"} 
+        {"name":"La Mer Eye Concentrate", "catalog":"Skincare / Eye", "price":"180", "dateofpurchase":"2017/03"}, 
+        {"name":"Cle De Peau Correcting Cream Veil", "catalog":"Cosmetics / Face", "price":"70", "dateofpurchase":"2017/03"},
+	    {"name":"Laura Mercier Pressed Powder", "catalog":"Cosmetics / Face", "price":"56", "dateofpurchase":"2017/03"},
+        {"name":"Giorgio Armani Eyeshaper", "catalog":"Cosmetics / Tool", "price":"25", "dateofpurchase":"2017/03"},
+        {"name":"Marc Jacobs Matte Eyeliner", "catalog":"Cosmetics / Eye", "price":"20", "dateofpurchase":"2017/03"},
+        {"name":"Fresh Deep Hydration Facial Toner", "catalog":"Skincare / Face", "price":"40", "dateofpurchase":"2017/04"}, 
+	    {"name":"Nars Dual-intensity Eyeshadow", "catalog":"Cosmetics / Eye", "price":"26", "dateofpurchase":"2017/04"} , 
+	    {"name":"givenchy ink fundation", "catalog":"Cosmetics / Face", "price":"60", "dateofpurchase":"2017/04"} , 
+	    {"name":"Sephora Colorful Face Powders", "catalog":"Cosmetics / Face", "price":"14", "dateofpurchase":"2017/04"} 
     ];
     $scope.productOwnedCharData = [{
         name: 'cosmetics / Eye',
@@ -62,14 +62,18 @@ app.controller("myCtrl", ['$scope', '$http',function($scope,$http) {
         else if($scope.productCatalog == ""){
             alert("Please choose Product catalog");
         }
+        else if(!isValidDate($scope.purchaseDate)){
+            alert("Please enter a valid date");
+            $scope.purchaseDate = "";
+        }
         else{
-        var today = new Date();
+        console.log($scope.purchaseDate);
            $scope.productOwned.push(
             {
                 "name":$scope.productName, 
                 "catalog":$scope.productCatalog.catalog, 
                 "price":$scope.productPrice, 
-                "dateofpurchase":today
+                "dateofpurchase":$scope.purchaseDate
             });
             $scope.showPurchaseForm = false;
             $scope.showPurchaseButton = true;
@@ -93,6 +97,17 @@ app.controller("myCtrl", ['$scope', '$http',function($scope,$http) {
         $scope.productPrice = 0;
         $scope.productCatalog = "";
         $scope.productReview = "";
+    }
+
+    function isValidDate(date){
+        if (date == "") return false;
+        var res = date.split("/");
+        if (res.length != 2) return false;
+        var year = parseInt(res[0]);
+        if (year != (new Date()).getFullYear()) return false;
+        var month = parseInt(res[1]);
+        if (month <1 || month > 12) return false;
+        return true;       
     }
 
     function drawHighChart(data){
